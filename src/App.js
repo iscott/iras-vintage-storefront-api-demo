4cff30270d0dc05128f389c5b3b7c5f2d9ee188e
Ira Herman <ira@xynetik.com>
2021-09-19 19:45:39 -0700

    Grabs products from shopify storefront API and console logs

--- src/App.js
+++ src/App.js
@@ -1,22 +1,64 @@
-import logo from './logo.svg';
+import {useEffect} from "react";
+import axios from "axios";
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    async function fetchData(){
+      const URL = 'https://iras-vintage-shop.myshopify.com/api/2021-07/graphql.json';
+      
+      let config = {
+        headers: {
+          "X-Shopify-Storefront-Access-Token": "d1233fd65218b41ad385b5f5049336fa",
+        }
+      };
+
+      const data = {
+        query: `
+          {
+            products(first: 10) {
+              edges {
+                cursor
+                node {
+                  id
+                  title
+                  description
+                  handle
+                  variants(first: 3) {
+                    edges {
+                      cursor
+                      node {
+                        id
+                        title
+                        quantityAvailable
+                        priceV2 {
+                          amount
+                          currencyCode
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        `
+      };
+
+      const products = await axios.post(URL, data, config);
+      console.log(products);
+    }
+  
+    fetchData();
+
+  })
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ira's Vintage Shop React Front End
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
