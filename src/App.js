23a5874636570a3bf71ccbe9bad1500121f4d6de
Ira Herman <ira@xynetik.com>
2021-09-20 20:06:54 -0700

    Adds Header component with nav and some products grid code to Aoo

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import {useEffect} from "react";
 import axios from "axios";
 import './App.css';
 
+import Header from './components/Header';
+
 function App() {
 
   const URL = 'https://iras-vintage-shop.myshopify.com/api/2021-07/graphql.json';
@@ -83,7 +85,42 @@ function App() {
     `
   };
 
+  // const products = [
+  //   {
+  //     id: 1,
+  //     name: 'Earthen Bottle',
+  //     href: '#',
+  //     price: '$48',
+  //     imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg',
+  //     imageAlt: 'Tall slender porcelain bottle with natural clay textured body and cork stopper.',
+  //   },
+  //   {
+  //     id: 2,
+  //     name: 'Nomad Tumbler',
+  //     href: '#',
+  //     price: '$35',
+  //     imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg',
+  //     imageAlt: 'Olive drab green insulated bottle with flared screw lid and flat top.',
+  //   },
+  //   {
+  //     id: 3,
+  //     name: 'Focus Paper Refill',
+  //     href: '#',
+  //     price: '$89',
+  //     imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg',
+  //     imageAlt: 'Person using a pen to cross a task off a productivity paper card.',
+  //   },
+  //   {
+  //     id: 4,
+  //     name: 'Machined Mechanical Pencil',
+  //     href: '#',
+  //     price: '$35',
+  //     imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
+  //     imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
+  //   },
+  // ]
 
+  const products = [];
   useEffect(() => {
     async function fetchData(){
       const response = await axios.post(URL, data, config);
@@ -97,11 +134,29 @@ function App() {
   
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Ira's Vintage Shop React Front End
-        </p>
-      </header>
+      <Header />
+      <h1 className="pt-12 text-4xl font-extrabold tracking-tight text-gray-900">Ira's Vintage Shop (React Front End)</h1>        
+      <div className="bg-white">
+        <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+          <h2 className="sr-only">Products</h2>
+
+          <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+            {products.map((product) => (
+              <a key={product.id} href={product.href} className="group">
+                <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+                  <img
+                    src={product.imageSrc}
+                    alt={product.imageAlt}
+                    className="w-full h-full object-center object-cover group-hover:opacity-75"
+                  />
+                </div>
+                <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
+                <p className="mt-1 text-lg font-medium text-gray-900">{product.price}</p>
+              </a>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
